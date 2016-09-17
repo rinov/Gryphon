7cbefa16c8d86bfaa9966124574135441bf77e2d
Ryo Ishikawa <rinov@rinov.jp>
2016-09-17 16:15:20 +0900

    Update Example/

---
+++ Example/Pods/Target Support Files/Gryphon/Gryphon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Gryphon : NSObject
+@end
+@implementation PodsDummy_Gryphon
+@end
---
+++ Example/Pods/Target Support Files/Gryphon/Gryphon-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double GryphonVersionNumber;
+FOUNDATION_EXPORT const unsigned char GryphonVersionString[];
+

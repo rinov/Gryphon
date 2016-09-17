da4e38b0c3a5bdd0e347925ae4e5161b99caceb4
Ryo Ishikawa <rinov@rinov.jp>
2016-09-17 18:29:07 +0900

    Update Tests.swift

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,32 +5,5 @@ import Nimble
 import Gryphon
 
 class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-        describe("TEST") {
-
-            it("is true") {
-                expect(1) == 1
-            }
-
-            
-            context("these will pass") {
-
-                it("will eventually pass") {
-
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
-        }
-    }
+    override func spec() {}
 }
